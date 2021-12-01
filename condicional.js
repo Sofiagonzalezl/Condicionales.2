b9275f2dece3cd50b9bb225cea13ceed2d60abc9
Sofiagonzalezl <92395715+Sofiagonzalezl@users.noreply.github.com>
2021-11-30 22:01:21 -0500

    Add files via upload

---
+++ condicional.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt("Ingrese el numero"))
+
+if(numero % 3== 0)
+(console.log ("Su número es divisible entre 3"))
+} else { 
+if { numero % 5 == 0 }
+{ console.log ("Su número es divisle entre 5")
+} else {
